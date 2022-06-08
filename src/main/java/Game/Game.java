383e7baa4fdac9bc1d309c6eec906e0fc394a5e2
Taha jalali <eparis@atlassian.comtaha.mtjn@gmail.com>
2022-06-08 18:19:53 +0430

    update

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,5 +1,25 @@
 package Game;
 
+import Model.Player.Player;
+
+import java.util.ArrayList;
+
 public class Game {
+
+    GameState gameState;
+
+    public Game(ArrayList<Player> players){
+        gameState = new GameState(players);
+    }
+
+    public void nextLevel(){
+        gameState.setLevel(gameState.getLevel()+1);
+        //todo get cards
+    }
+
+    public void reduceLive(){
+        gameState.setLives(gameState.getLives()-1);
+    }
+
 
 }
--- src/main/java/Game/GameState.java
+++ src/main/java/Game/GameState.java
@@ -12,4 +12,45 @@ public class GameState {
 
     private int ninjas;
 
+    private int level;
+
+    public GameState(ArrayList<Player> players){
+        this.players =players;
+        lives = players.size();
+        ninjas=2;
+        level=1;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getNinjas() {
+        return ninjas;
+    }
+
+    public void setNinjas(int ninjas) {
+        this.ninjas = ninjas;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
 }
